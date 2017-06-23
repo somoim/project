f766627d53cc51e4b1bf7f9668acc3a02731c7be
hgkim <gusrms126@naver.com>
2017-06-23 17:32:03 +0900

    로그인 기능추가

--- Somoim/src/main/java/kr/co/controller/CrewController.java
+++ Somoim/src/main/java/kr/co/controller/CrewController.java
@@ -5,17 +5,22 @@ import java.util.List;
 
 import javax.annotation.Resource;
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import kr.co.domain.ChattingVO;
 import kr.co.domain.CrewVO;
@@ -159,12 +164,16 @@ public class CrewController {
 	
 	///////////////////////// 진희
 	@RequestMapping(value="/list", method=RequestMethod.GET)
-	public void crew_list(@ModelAttribute("cri") Criteria cri, Model model, String mid) throws Exception {
-		// 로그인 한 mid 넘겨주세요
-		mid = "user10";
+	public void crew_list(@ModelAttribute("cri") Criteria cri, Model model, HttpServletRequest request,  HttpServletResponse response, ModelAndView modelAndView) throws Exception {
+		
+		HttpSession session = request.getSession();
+		MemberVO memberVO = (MemberVO) session.getAttribute("login");
+		String mid = memberVO.getMid();
+
 		List<CrewVO> list = crew_service.crew_list(cri, mid);
 		model.addAttribute("list", list);
 	}
+	
 	
 	@ResponseBody
 	@RequestMapping(value="/list_sList/{cno}")
--- Somoim/src/main/webapp/WEB-INF/views/header.jsp
+++ Somoim/src/main/webapp/WEB-INF/views/header.jsp
@@ -1,11 +1,10 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
     
-    
 <header id="header">
 	<div class="userCont">
 		<span class="glyphicon glyphicon-user"></span>
-		<span>사용자</span>
+		<span>${login.mid}</span>
 	</div>
 	<div class="searchCont">
 		<span class="glyphicon glyphicon-search"></span>
